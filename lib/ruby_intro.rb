45374cd06ea36da8a886ad0029b37c272852de90
brandon <beven@uccs.edu>
2018-09-08 17:26:07 +0000

    Added book in stock class with functions

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -41,18 +41,74 @@ end
 
 def hello(name)
   # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant?(s)
+  consonants = "bcdfghjklmnpqrstvwxyz".chars
+  if s.empty? == false
+    if consonants.include?(s[0].downcase)
+        return true
+    else
+        return false
+    end
+  else
+    return false
+  end
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+def binary_multiple_of_4?(s)
+  binstr = s.upack('B*')
+  if binstr.count('01') == binstr.size
+    if /^[10]*00$/.match(binstr)
+      return true
+    else
+      return false
+    end
+  else
+    puts "'#{s}' is not a valid binary number!"
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+
+    def initialize(isbn, price)
+    
+        if isbn.empty? == true
+            raise ArgumentError, "empty array"
+        else
+            @isbn = isbn
+        end
+        
+        if price <= 0
+            raise ArgumentError, "price less than zero"
+        else
+            @price = price
+        end
+    end
+    
+    def price=(new_price)
+        @price = new_price
+    end
+    
+    def price
+        return @price
+    end
+    
+    def isbn=(new_isbn)
+        @isbn = new_isbn
+    end
+    
+    def isbn
+        return @isbn
+    end
+    
+    def price_as_string()
+        format = "%0.2f" % @price
+        dollar_value, cent_value = format.split '.'
+        return "$#{dollar_value}.#{cent_value}"
+    end
 end
